ac2cb6457a031b1a987413d7fc6003c3d1fe51c3
Dan Lewis <48622577+danjlewis@users.noreply.github.com>
2023-06-29 19:11:47 +0100

    implement dispatch error handling

--- src/commands.rs
+++ src/commands.rs
@@ -25,4 +25,78 @@ pub fn framework() -> StandardFramework {
         .configure(|cfg| cfg.prefix(COMMAND_PREFIX))
         .group(&general::GENERAL_GROUP)
         .help(&help::HELP)
+        .on_dispatch_error(dispatch_error_hook)
+}
+
+#[hook]
+async fn dispatch_error_hook(
+    ctx: &Context,
+    msg: &Message,
+    error: DispatchError,
+    command_name: &str,
+) {
+    match error {
+        DispatchError::NotEnoughArguments { min, given } => {
+            let reply_content = format!(
+                "`{COMMAND_PREFIX}{command_name}` requires {min} arguments, but only received {given}."
+            );
+            let reply = msg.reply_ping(ctx, reply_content).await;
+
+            if reply.is_err() {
+                error!("Unhandled dispatch error in {}: {:?}", command_name, error)
+            }
+        }
+        DispatchError::TooManyArguments { max, given } => {
+            let reply_content = format!(
+                "`{COMMAND_PREFIX}{command_name}` only accepts {max} arguments, but received {given}."
+            );
+            let reply = msg.reply_ping(ctx, reply_content).await;
+
+            if reply.is_err() {
+                error!("Unhandled dispatch error in {}: {:?}", command_name, error)
+            }
+        }
+        DispatchError::OnlyForDM => {
+            let reply_content =
+                format!("`{COMMAND_PREFIX}{command_name}` can only be used in DMs.");
+            let reply = msg.reply_ping(ctx, reply_content).await;
+
+            if reply.is_err() {
+                error!("Unhandled dispatch error in {}: {:?}", command_name, error)
+            }
+        }
+        DispatchError::OnlyForGuilds => {
+            let reply_content =
+                format!("`{COMMAND_PREFIX}{command_name}` can only be used in servers.");
+            let reply = msg.reply_ping(ctx, reply_content).await;
+
+            if reply.is_err() {
+                error!("Unhandled dispatch error in {}: {:?}", command_name, error)
+            }
+        }
+        DispatchError::OnlyForOwners
+        | DispatchError::LackingRole
+        | DispatchError::LackingPermissions(_) => {
+            let reply_content =
+                format!("You don't have permission to use `{COMMAND_PREFIX}{command_name}`.");
+            let reply = msg.reply_ping(ctx, reply_content).await;
+
+            if reply.is_err() {
+                error!("Unhandled dispatch error in {}: {:?}", command_name, error)
+            }
+        }
+        DispatchError::Ratelimited(_) => {
+            let reply = msg
+                .reply_ping(ctx, "Rate limit reached, please try again soon.")
+                .await;
+
+            if reply.is_err() {
+                error!("Unhandled dispatch error in {}: {:?}", command_name, error);
+            }
+        }
+        DispatchError::BlockedUser
+        | DispatchError::BlockedGuild
+        | DispatchError::BlockedChannel => {}
+        _ => error!("Unhandled dispatch error in {}: {:?}", command_name, error),
+    }
 }
